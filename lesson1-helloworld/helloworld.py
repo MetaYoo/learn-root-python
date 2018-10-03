c4db0931b1c59652a2cd319eb0e82e94edaf862b
aracwong <aracwong@163.com>
2018-10-04 07:04:28 +0800

    initial hello world project

---
+++ lesson1-helloworld/helloworld.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# coding=utf-8
+print('hello world !')
+
+
+def main():
+    """
+    this is main method
+    do nothing now
+    :return: void
+    """
+    ...
+
+
+if __name__ == '__main__':
+    main()
